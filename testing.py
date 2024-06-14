0639328a18a65c237f9b41b82eadd8edac0238e1
James Hatt <144613957+jhattenhauer@users.noreply.github.com>
2024-06-14 15:31:33 -0230

    rotate around non-global orgin
    figure out how to not rotate around global origin but around specified

--- testing.py
+++ testing.py
@@ -19,21 +19,23 @@ class arm1(Entity):
         self.model= "cube"
         self.texture = "white_cube"
         self.scale = (0.35,1,0.35)
-        
+        self.origin = (0,-1,0)
+
     def update(self):
         self.rotation_y += held_keys['d'] * time.dt * 100 #stacking commands with children allow for rotation of whole tower
         self.rotation_y -= held_keys['a'] * time.dt * 100
         self.rotation_y += held_keys['c'] * time.dt * 100
         self.rotation_y -= held_keys['z'] * time.dt * 100
-    def input(self, key):
+        
+    def input(self, key): #look into the math that's happening here
         if key == "w":
-            self.rotate((self.rotation_x + 1,0,0), base_block)
+            self.rotate(Vec3(0,0,1))
+            print(arm.rotation_getter())
         if key == "s":
-            self.rotate((self.rotation_x - 10,0,0), base_block)
-
-
+            self.rotate(Vec3(0,0,-1))
+            print(arm.rotation_getter())
 base_block = base()   
 arm = arm1()
 
-arm.origin = (0,-1,0)     
+
 app.run()
