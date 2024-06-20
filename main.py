751d7b64caeac3f8a5c5c0b7440668d8742b4a4c
James Hatt <144613957+jhattenhauer@users.noreply.github.com>
2024-06-20 15:35:44 -0230

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class hinge(Entity):
         super().__init__(add_to_scene_entities, enabled, **kwargs)
         self.model="Cube"
         self.texture = "white_cube"
+        
 
 #rotation functions
 
@@ -29,67 +30,23 @@ def spin3():
     hinge3.rotation_z += 100 * (held_keys["9"] - held_keys["6"]) * time.dt
 
 def spin4():
-    hinge4.rotation_z += 100 * (held_keys["w"] - held_keys["s"]) * time.dt
+    
     hinge4.rotation_y += 100 * (held_keys["a"] - held_keys["d"]) * time.dt
 
 #building arm    
-base = hinge()
-base.position = (0,-4,0)
-base.update = spin
+base = hinge(position = (0,-4,0), update = spin,)
+arm1 = arm(parent = base, position = (0,1,0), visible = True)
+hinge1 = hinge(parent = arm1, position = (0,1,0), update = spin1, visible = True)
+arm2 = arm(parent = hinge1, position = (0,1,0), visible = True)
+hinge2 = hinge(parent = arm2, position = (0,1,0), update = spin2, visible = True)
+arm3 = arm(parent = hinge2, position = (0,1,0), visible = True)
+hinge3 = hinge(parent = arm3, position = (0,1,0), update = spin3, visible = True)
+arm4 = arm(parent = hinge3, position = (0,1,0), visible = True)
+hinge4 = hinge(parent = arm4, position = (0,1,0), update = spin4, visible = True)
 
-arm1 = arm()
-arm1.parent = base
-arm1.position = (0,1,0)
-
-hinge1 = hinge()
-hinge1.parent = arm1
-hinge1.position = (0,1,0)
-hinge1.update = spin1
-
-arm2 = arm()
-arm2.parent = hinge1
-arm2.position = (0,1,0)
-
-hinge2 = hinge()
-hinge2.parent = arm2
-hinge2.position = (0,1,0)
-hinge2.update = spin2
-
-arm3 = arm()
-arm3.parent = hinge2
-arm3.position = (0,1,0)
-
-hinge3 = hinge()
-hinge3.parent = arm3
-hinge3.position = (0,1,0)
-hinge3.update = spin3
-
-arm4 = arm()
-arm4.parent = hinge3
-arm4.position = (0,1,0)
-
-hinge4 = hinge()
-hinge4.parent = arm4
-hinge4.position = (0,1,0)
-hinge4.update = spin4
-
-
-
-finger = hinge()
-finger.parent = hinge4
-finger.scale = (0.2,1,0.2)
-finger.position = (0.5,0.5,0)
-finger2 = hinge()
-finger2.parent = hinge4
-finger2.scale = (0.2,1,0.2)
-finger2.position = (-0.5,0.5,0)
-finger3 = hinge()
-finger3.parent = hinge4
-finger3.scale = (0.2,1,0.2)
-finger3.position = (0,0.5,0.5)
-finger4 = hinge()
-finger4.parent = hinge4
-finger4.scale = (0.2,1,0.2)
-finger4.position = (0,0.5,-0.5)
+finger = hinge(parent = hinge4, scale = (0.2,1,0.2),position = (0.5,0.5,0), visible = True)
+finger2 = hinge(parent = hinge4, scale = (0.2,1,0.2), position = (-0.5,0.5,0), visible = True)
+finger3 = hinge(parent = hinge4, scale = (0.2,1,0.2), position = (0,0.5,0.5), visible = True)
+finger4 = hinge(parent = hinge4, scale = (0.2,1,0.2), position = (0,0.5,-0.5), visible = True)
 
 app.run()
